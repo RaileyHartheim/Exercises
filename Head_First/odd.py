5c0c634ad26ab1e5cf4dbdb761879ba872a700f0
Yulia Kazhaeva <melancholia.raiven@gmail.com>
2020-01-26 18:34:41 +0500

    added cycle, random time between messages

--- Head_First/odd.py
+++ Head_First/odd.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+import time
+import random
 
-right_this_minute = datetime.today().minute
+for i in range(5):
+    right_this_minute = datetime.today().minute
 
-if right_this_minute % 2 == 1:
-    print("This minute seems a little odd")
-else:
-    print("Not an odd minute.")
+    if right_this_minute % 2 == 1:
+        print("This minute seems a little odd")
+    else:
+        print("Not an odd minute.")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
