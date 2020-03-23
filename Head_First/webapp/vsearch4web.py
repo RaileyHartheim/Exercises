196f4b5958500f73834987e92ea16a294654bded
Yulia Kazhaeva <melancholia.raiven@gmail.com>
2020-03-23 10:43:12 +0500

    added logging

--- Head_First/webapp/vsearch4web.py
+++ Head_First/webapp/vsearch4web.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, escape
 from search_functions import search_for_letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return('Hello world from Flask!')
+def log_request(req: 'flask_request', res:str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
 
+
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title = 'Welcome to search4letters on the web!')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search_for_letters('life, the universe, and everything!', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results: '
+    results = str(search_for_letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                            the_phrase=phrase,
+                            the_letters=letters,
+                            the_title=title,
+                            the_results=results,)
 
-app.run(debug=True)
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
+
+if __name__ == '__main__':
+    app.run(debug=True)
